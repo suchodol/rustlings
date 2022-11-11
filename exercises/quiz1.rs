ed88688e70e11e3f8ac8d3dde2e67a0dcba4ab21
Suchy7 <suchycmg@gmail.com>
2022-11-11 20:45:42 +0000

    rusting...

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+
+fn calculate_price_of_apples(quantity: i32) -> i32{
+    if(quantity > 40){
+        quantity
+    }else{
+        quantity * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
@@ -28,3 +34,4 @@ fn verify_test() {
     assert_eq!(41, price3);
     assert_eq!(65, price4);
 }
+
